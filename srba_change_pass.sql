716d34793adaea181e5d8a9df4ba4250954b3d91
Jiri Srba <jsrba@csas.cz>
2018-01-23 10:19:07 +0100

    Git daily snapshot on _1019

--- srba_change_pass.sql
+++ srba_change_pass.sql
@@ -1,6 +1,7 @@
 --// change password/lock of user SRBA //--
 
 -- SQL verze
+
 /*
 def username=SRBA
 
@@ -19,34 +20,38 @@ select username, account_status from dba_users where username = '&username';
 
 
 -- PL/SQL verze
+
+def username=SRBA
+
 set serveroutput on
+
 
 DECLARE
   v_if_exist NUMBER;
   v_username dba_users.username%type;
 BEGIN
   -- SOL60210
-  v_username := 'SOL60210';
-  SELECT COUNT(*) INTO v_if_exist 
+  v_username := '&username';
+  SELECT COUNT(*) INTO v_if_exist
      FROM dba_users  WHERE username = v_username;
-  
+
   if v_if_exist > 0 THEN
     execute immediate 'drop user '||v_username||' cascade';
   END IF;
-  
+
   v_username := 'SRBA';
-  SELECT COUNT(*) INTO v_if_exist 
+  SELECT COUNT(*) INTO v_if_exist
      FROM dba_users  WHERE username = v_username;
-  
+
   if v_if_exist = 0 THEN
     execute immediate 'create user '||v_username||' IDENTIFIED BY abcd1234';
-  END IF;  
+  END IF;
 
   -- change password
   execute immediate 'alter user '||v_username||' IDENTIFIED BY "'||
       dbms_random.string('a',14)||ABS(trunc(dbms_random.value(0, 9)))||'"';
-  -- lock account, profile DBA    
-  execute immediate 'alter user '||v_username||' account lock';  
+  -- lock account, profile DBA
+  execute immediate 'alter user '||v_username||' account lock';
   execute immediate 'alter user '||v_username||' profile PROF_DBA';
   execute immediate 'grant create session to '||v_username;
   execute immediate 'revoke dba from '||v_username;
