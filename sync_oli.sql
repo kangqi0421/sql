448202d78c856bf05ec9e52aabbf0744c442bd20
Jiri Srba <jsrba@csas.cz>
2018-10-15 08:54:07 +0200

    Git daily snapshot on _0853

--- sync_oli.sql
+++ sync_oli.sql
@@ -1,5 +1,12 @@
 -- SYN data from OEM
 
+-- OEM to OLI
+BEGIN
+  OLI_OWNER.SYNCHRO_OEM.resync_to_oli;
+END;
+/
+
+-- refresh server
 BEGIN
   DASHBOARD.REFRESH_OLI_DBHOST_PROPERTIES;
 END;
@@ -10,3 +17,5 @@ BEGIN
 DBMS_SNAPSHOT.REFRESH('DASHBOARD.API_DB_MV','C');
 END;
 /
+
+commit;
