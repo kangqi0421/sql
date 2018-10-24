4d4d538d2959812ce9e278d2ec14c5435ddf8c8f
Jiri Srba <jsrba@csas.cz>
2018-10-24 12:23:02 +0200

    Git daily snapshot on _1221

--- temp_sort.sql
+++ temp_sort.sql
@@ -13,6 +13,13 @@ order by actual_mem_used;
 select * from dba_tablespaces
  where contents = 'TEMPORARY';
 
+select s.username, round(sum(s.blocks * value)/power(1024,3)) "GB"
+  from GV$TEMPSEG_USAGE s, v$parameter p
+    where name = 'db_block_size'
+  group by s.username
+  order by 2 desc;
+
+-- starsi verze 12
 select s.sid, s.serial#, s.username,
        sort.username, s.state, s.event,
        s.logon_time,
@@ -22,6 +29,7 @@ select s.sid, s.serial#, s.username,
   where tablespace = 'TEMP'
    -- and s.username = 'DAMI_ETL_OWNER'
 ;
+
 
 with temp as (
 select username, sql_id_tempseg, blocks*block_size bs
