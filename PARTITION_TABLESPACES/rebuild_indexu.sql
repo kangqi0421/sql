57c5668a0ead3b337af3d6a3034f8611ec8c8419
Jiri Srba <jsrba@csas.cz>
2018-11-14 09:20:06 +0100

    Git daily snapshot on _0919

--- PARTITION_TABLESPACES/rebuild_indexu.sql
+++ PARTITION_TABLESPACES/rebuild_indexu.sql
@@ -1,4 +1,42 @@
 
+
+  PROCEDURE rebuild_indexes (
+      p_parallel_level NUMBER := 32
+  )
+  AS
+  BEGIN
+    -- rebuild GLOBAL indexes
+    for rec in (
+        select index_owner, index_name, partition_name, 'PARTITION' ddl_type
+        from all_ind_partitions
+        where index_owner in (select distinct table_owner FROM load_table)
+          and status = 'UNUSABLE'
+        union all
+        select index_owner, index_name, subpartition_name, 'SUBPARTITION' ddl_type
+        from all_ind_subpartitions
+        where index_owner in (select distinct table_owner FROM load_table)
+         and status = 'UNUSABLE'
+        union all
+        select owner, index_name, null, null
+        from all_indexes
+        where owner in (select distinct table_owner FROM load_table)
+        and status = 'UNUSABLE'
+    )
+    LOOP
+      if rec.ddl_type is null then
+        execute immediate 'alter index '
+            ||dbms_assert.enquote_name(rec.index_owner)
+            ||'.'||dbms_assert.enquote_name(rec.index_name)||' rebuild nologging parallel '
+            || p_parallel_level;
+      else
+        execute immediate 'alter index '
+            ||dbms_assert.enquote_name(rec.index_owner)
+            ||'.'||dbms_assert.enquote_name(rec.index_name)
+            ||' rebuild '||rec.ddl_type||' '||rec.partition_name;
+      end if;
+    END LOOP;
+  END;
+
 CREATE OR REPLACE PACKAGE maint_part
 IS
 --   PROCEDURE createpartition (tbl_owner IN VARCHAR2);
@@ -41,9 +79,9 @@ IS
       THEN
          DBMS_OUTPUT.put_line (TO_CHAR (SQLCODE) || ' - ' || SQLERRM);
    END rebuildlocalindexes;
-   
+
    /* ------------------------------------------------------------------------- */
-   
+
    PROCEDURE rebuildglobalindexes (idx_owner IN VARCHAR2)
    IS
       --
@@ -75,6 +113,6 @@ IS
       THEN
          DBMS_OUTPUT.put_line (TO_CHAR (SQLCODE) || ' - ' || SQLERRM);
    END rebuildglobalindexes;
-   
+
 END maint_part;
 /
