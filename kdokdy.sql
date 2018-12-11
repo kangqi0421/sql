4dc38c00d056f7cd2fd7194b212a84db22df71f7
Jiri Srba <jsrba@csas.cz>
2018-12-11 09:19:00 +0100

    Git daily snapshot on _0917

--- kdokdy.sql
+++ kdokdy.sql
@@ -17,14 +17,14 @@ select /*+FULL(a) */
   from ARM12.ARM_UNIAUD12 a
  where ARM_FULLID in (
       select ARM_FULLID from ARM_ADMIN.ARM_DATABASES
-          where arm_db_name like '&db' and TRANSFER_ENABLED = 'Y')
+          where arm_db_name like upper('&db') and TRANSFER_ENABLED = 'Y')
   AND ARM_timestamp > SYSTIMESTAMP - INTERVAL '1' DAY
 --     and ARM_TIMESTAMP between TIMESTAMP'2017-08-22 17:00:00'
   --                         and TIMESTAMP'2017-08-22 18:10:00'
       and ARM_ACTION_NAME='LOGON'
-      -- and dbusername like upper('&user')
-      and dbusername in ('CPT','CPT_APP','CPTPK','CPTPK_APP','JOB_APP')
---    and return_code > 0
+      and dbusername like upper('&user')
+--      and dbusername in ('CPT','CPT_APP','CPTPK','CPTPK_APP','JOB_APP')
+      and return_code > 0
       and return_code in (1017)
 ORDER by event_timestamp DESC
 FETCH FIRST 10 ROWS ONLY
