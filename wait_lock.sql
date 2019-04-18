ab9ceb184165cfae007dff4398c6d916b5520c8f
Jiri Srba <jsrba@csas.cz>
2019-04-18 07:38:31 +0200

    Git daily snapshot on 2019-04-18T073748

--- wait_lock.sql
+++ wait_lock.sql
@@ -2,19 +2,24 @@
 -- LOCK and WAITs
 --
 
+-- Chyba: ​ORA-00054: resource busy and acquire with NOWAIT specified or timeout expired
+
+alter session set ddl_lock_timeout=60;
+
+
 -- Get the final blocker sessions in Oracle
 select distinct FINAL_BLOCKING_SESSION from v$session where  FINAL_BLOCKING_SESSION_STATUS='VALID';
 
 
 -- detailed info
- SELECT 
+ SELECT
      chain_id, num_waiters, in_wait_secs, osid, blocker_osid, wait_event_text
-   FROM v$wait_chains; 
+   FROM v$wait_chains;
 
 -- TOP 100
-SELECT * 
-FROM (SELECT 'Current Process: '||osid W_PROC, 'SID '||i.instance_name INSTANCE, 
-'INST #: '||instance INST,'Blocking Process: '||decode(blocker_osid,null,'<none>',blocker_osid)|| 
+SELECT *
+FROM (SELECT 'Current Process: '||osid W_PROC, 'SID '||i.instance_name INSTANCE,
+'INST #: '||instance INST,'Blocking Process: '||decode(blocker_osid,null,'<none>',blocker_osid)||
 ' from Instance '||blocker_instance BLOCKER_PROC,'Number of waiters: '||num_waiters waiters,
 'Wait Event: ' ||wait_event_text wait_event, 'P1: '||p1 p1, 'P2: '||p2 p2, 'P3: '||p3 p3,
 'Seconds in Wait: '||in_wait_secs Seconds, 'Seconds Since Last Wait: '||time_since_last_wait_secs sincelw,
@@ -30,13 +35,13 @@ ORDER BY chain_id,
 num_waiters DESC)
 WHERE ROWNUM <= 10;
 
-SELECT * 
-FROM (SELECT 'Current Process: '||osid W_PROC, 'SID '||i.instance_name INSTANCE, 
- 'INST #: '||instance INST,'Blocking Process: '||decode(blocker_osid,null,'<none>',blocker_osid)|| 
+SELECT *
+FROM (SELECT 'Current Process: '||osid W_PROC, 'SID '||i.instance_name INSTANCE,
+ 'INST #: '||instance INST,'Blocking Process: '||decode(blocker_osid,null,'<none>',blocker_osid)||
  ' from Instance '||blocker_instance BLOCKER_PROC,
  'Number of waiters: '||num_waiters waiters,
  'Final Blocking Process: '||decode(p.spid,null,'<none>',
- p.spid)||' from Instance '||s.final_blocking_instance FBLOCKER_PROC, 
+ p.spid)||' from Instance '||s.final_blocking_instance FBLOCKER_PROC,
  'Program: '||p.program image,
  'Wait Event: ' ||wait_event_text wait_event, 'P1: '||wc.p1 p1, 'P2: '||wc.p2 p2, 'P3: '||wc.p3 p3,
  'Seconds in Wait: '||in_wait_secs Seconds, 'Seconds Since Last Wait: '||time_since_last_wait_secs sincelw,
