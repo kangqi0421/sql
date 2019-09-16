68e4bb45ea9925170b6d99104041564f29793ce9
Jiri Srba <jsrba@csas.cz>
2019-09-16 09:25:53 +0200

    Git daily snapshot on 2019-09-16T092448

--- CREATE_DB/default_profile.sql
+++ CREATE_DB/default_profile.sql
@@ -13,22 +13,25 @@ END;
 /
 
 -- expire default passwords
-BEGIN
-  FOR rec IN
-  (
-    SELECT DISTINCT u.name username
-      FROM SYS.user$ u          -- vsechny existujici ucty
-      JOIN SYS.default_pwd$ dp  -- vsechny registrovane default ucty
-      ON (u.name        = dp.user_name)
-      WHERE u.type#     = 1
-        AND u.name NOT IN ('XS$NULL', 'OJVMSYS') -- ucet fyzicky neexistuje
-        AND u.astatus   > 0           -- OPEN ucty nezamykej
-  )
-  LOOP
-    -- expire, lock and change password to random
-    EXECUTE immediate ('alter user "'||rec.username ||'" identified by "'||
-    dbms_random.string('a',14)||ABS(TRUNC(dbms_random.value(0, 9))) ||'"'||
-    ' password expire account lock');
-  END LOOP;
-END;
-/
+-- zruseno, pro nove verze hlasi chybu
+-- ORA-28010: cannot expire external users, global users, or users with no authentication method
+-- BEGIN
+--   FOR rec IN
+--   (
+--     SELECT DISTINCT u.name username
+--       FROM SYS.user$ u          -- vsechny existujici ucty
+--       JOIN SYS.default_pwd$ dp  -- vsechny registrovane default ucty
+--       ON (u.name        = dp.user_name)
+--       WHERE u.type#     = 1
+--         AND u.name NOT IN ('XS$NULL', 'OJVMSYS') -- ucet fyzicky neexistuje
+--         AND u.astatus   > 0           -- OPEN ucty nezamykej
+--   )
+--   LOOP
+--     -- expire, lock and change password to random
+--     EXECUTE immediate (
+--         'alter user "'||rec.username ||'" identified by "'||
+--         dbms_random.string('a',14)||ABS(TRUNC(dbms_random.value(0, 9))) ||'"'||
+--         ' password expire account lock');
+--   END LOOP;
+-- END;
+-- /
--- CREATE_DB/flashback_on.sql
+++
@@ -1,5 +0,0 @@
--- enable flashback database ON
-
-WHENEVER SQLERROR EXIT SQL.SQLCODE
-
-alter database flashback on;
